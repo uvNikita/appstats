7591e807f7147b3fc7ac4f32a33639351c1448fe
Nikita Uvarov <uv.nikita@gmail.com>
2012-06-15 14:02:26 +0300

    Add 'number of lines in table' links

--- statistics/app.py
+++ statistics/app.py
@@ -59,6 +59,7 @@ def main_page():
 
     sort_by_field = request.args.get('sort_by_field', 'NUMBER')
     sort_by_period = request.args.get('sort_by_period', 'hour')
+    number_of_lines = request.args.get('number_of_lines', 20, int)
     if sort_by_field == 'NAME':
         get_sorting_key = lambda tpl: tpl[0]
     else:
