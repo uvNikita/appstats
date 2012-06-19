1c6f7bfb12c6033463237fa64368e584e2453f62
Nikita Uvarov <uv.nikita@gmail.com>
2012-06-19 17:10:45 +0300

    Remove cache field

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 
 REDIS_KEYS_PREFIX = 'appstats'
-FIELDS = ['cpu_time', 'real_time', 'sql', 'solr', 'redis', 'cache', 'memcached', 'NUMBER']
+FIELDS = ['cpu_time', 'real_time', 'sql', 'solr', 'redis', 'memcached', 'NUMBER']
