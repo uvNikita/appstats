80c979d31ce571a1cea1f6f07dac06775ddf2202
Nikita Uvarov <uv.nikita@gmail.com>
2016-09-01 22:38:12 +0200

    Install rmongodb from github instead of cran packages

--- init.R
+++ init.R
@@ -3,4 +3,5 @@ repos = "https://cran.rstudio.com"
 
 install.packages("devtools", lib=lib_dir, repos=repos)
 devtools::install_github("twitter/AnomalyDetection", lib=lib_dir, repos=repos)
-install.packages("rmongodb", lib=lib_dir, repos=repos)
+#install.packages("rmongodb", lib=lib_dir, repos=repos)
+devtools::install_github("mongosoup/rmongodb")
