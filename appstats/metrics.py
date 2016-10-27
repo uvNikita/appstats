4ee3f5f6a62945bf78e337756ee955cc41abbc4d
Nikita Uvarov <uv.nikita@gmail.com>
2016-10-27 10:50:48 +0200

    Add missing file

---
+++ appstats/metrics.py
@@ -0,0 +1,26 @@
+import cantal
+
+requests = cantal.RequestTracker('requests')
+post = cantal.RequestTracker('post')
+adding_stats_flow = cantal.Fork(['app', 'incrby', 'redis'],
+                                state='appstats.adding_stats')
+request_flow = cantal.Fork(['app', 'mongo'],
+                           state='appstats.processing_request')
+
+
+def request_tracking_middleware(wsgi_app):
+    def middleware(environ, start_response):
+        method = environ['REQUEST_METHOD']
+        if method == 'POST':
+            with (requests.request(),
+                  post.request(),
+                  adding_stats_flow.context()):
+                adding_stats_flow.app.enter()
+                return wsgi_app(environ, start_response)
+        else:
+            with requests.request(), request_flow.context():
+                request_flow.app.enter()
+                return wsgi_app(environ, start_response)
+    return middleware
+
+
