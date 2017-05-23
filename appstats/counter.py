0242377a5d56065faaac84837ac4848564455a59
Nikita Uvarov <uv.nikita@gmail.com>
2017-05-23 14:10:02 +0200

    Change update log message level to debug

--- appstats/counter.py
+++ appstats/counter.py
@@ -389,7 +389,7 @@ class PeriodicCounter(object):
     def update(self):
         prev_upd_key = self._make_key(self.prev_upd_key_format)
         prev_upd = self.redis_db.get(prev_upd_key)
-        log.info(
+        log.debug(
             "PeriodicCounter (collection: {collection}) "
             "update was triggered, previous update: {prev}".format(
                 collection=self.collection.name, prev=prev_upd,
